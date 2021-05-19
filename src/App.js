8b8042f27e37683c90b728ccf6426c5176adc6e4
Najeh Rahali <Rahali.najeeh@gmail.com>
2021-05-19 10:25:45 +0100

    form checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import * as ReactBootstrap from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <ReactBootstrap.Form>
+  <ReactBootstrap.Form.Group controlId="formBasicEmail">
+    <ReactBootstrap.Form.Label>Email address</ReactBootstrap.Form.Label>
+    <ReactBootstrap.Form.Control type="email" placeholder="Enter email" />
+    <ReactBootstrap.Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </ReactBootstrap.Form.Text>
+  </ReactBootstrap.Form.Group>
+
+  <ReactBootstrap.Form.Group controlId="formBasicPassword">
+    <ReactBootstrap.Form.Label>Password</ReactBootstrap.Form.Label>
+    <ReactBootstrap.Form.Control type="password" placeholder="Password" />
+  </ReactBootstrap.Form.Group>
+  <ReactBootstrap.Form.Group controlId="formBasicCheckbox">
+    <ReactBootstrap.Form.Check type="checkbox" label="Check me out" />
+  </ReactBootstrap.Form.Group>
+  <ReactBootstrap.Button variant="primary" type="submit">
+    Submit
+  </ReactBootstrap.Button>
+</ReactBootstrap.Form>
     </div>
   );
 }
